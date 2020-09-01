6f4ae9c73dfc5358c81d52fefabf3f187e3f4cfa
bofmar <bofmar@outlook.com>
2020-09-01 20:46:19 +0300

    final

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a training excersise that was made while following The Net Ninja's tutorial on regular expressions.
+
+The goal of this excersise is the creation of a form that takes input from the user and then uses Regex to validate that input, giving feedback to the user along the way.
+
+The exercise demonstrates the use of HTML5, CSS3 and JavaScript for the creation of said form. 
---
+++ validation.js
@@ -0,0 +1,27 @@
+//Validation script
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^[a-z\d\.-]+@[a-z\d\.-]+(\.[a-z]{2,8}){1,2}$/,
+    password: /^[\w@-]{8,20}$/i,
+    telephone:/^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
+};
+
+//validation function
+function validate (field,regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    }
+    else{
+        field.className = 'invalid';
+    }
+};
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',(e)=>{
+        //console.log(e.target.attributes.name.value);
+        validate(e.target,patterns[e.target.attributes.name.value]);
+    });
+});
